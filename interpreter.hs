308b42e6d8be0723e31aefc1e8759920a307131c
Marek Żochowski <marek@zochowski.net>
2020-05-01 17:48:58 +0200

    Initial commit

---
+++ interpreter.hs
@@ -0,0 +1,19 @@
+import System.Environment
+import System.IO
+import Control.Monad.Except
+
+
+getProgramPath :: IO String
+getProgramPath = do
+  args <- getArgs
+  case args of
+    [] -> error "ERROR: Interpreter got 0 arguments, but it expected exactly 1!"
+    l@(h:h2:t) -> error $ "ERROR: Intepreter got " ++ (show $ length l) ++ " arguments, but it expected exactly 1!"
+    (h:t) -> return h
+
+main :: IO ()
+main = do
+  programPath <- getProgramPath
+  withFile programPath ReadMode (\handle -> do
+    program <- hGetContents handle
+    print program)
